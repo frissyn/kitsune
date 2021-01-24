336e795d73e0fe82fc7365c0213b2df83987ecc5
frissyn <Ire.Raufu76@gmail.com>
2021-01-24 15:31:27 +0000

    🔨: fix token grant and content type

--- kitsune/auth.py
+++ kitsune/auth.py
@@ -1,14 +1,13 @@
-import json
 import requests
 
 from .values import url
 
 
 def get_token(username: str, password: str) -> dict:
-    data = {"username": username, "password": password, "grant_type": "password"}
+    data = {"email": username, "password": password, "grant_type": "password"}
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
-    headers = {"Content-Type": "application/json"}
-    r = requests.post(url("oauth"), json=json.dumps(data), headers=headers)
+    r = requests.post(url("oauth"), data=data, headers=headers)
 
     r.raise_for_status()
     return r.json()
@@ -16,9 +15,9 @@ def get_token(username: str, password: str) -> dict:
 
 def refresh_token(token: str) -> dict:
     data = {"refresh_token": token, "grant_type": "refresh_token"}
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
-    headers = {"Content-Type": "application/json"}
-    r = requests.post(url("oauth"), json=json.dumps(data), headers=headers)
+    r = requests.post(url("oauth"), data=data, headers=headers)
 
     r.raise_for_status()
     return r.json()
